3fb3c4888abed1248d35383cd6193c1e86d07693
root <ryanb214598@hotmail.com>
2023-03-29 08:58:32 +0000

    install notes

---
+++ install.sh
@@ -0,0 +1,21 @@
+apt install -y linux-headers-$(uname -r)
+wait
+sleep 1
+apt install -y iptables-dev make gcc pkg-config git
+wait
+sleep 1
+apt install dkms
+git clone https://github.com/Lochnair/xt_tls.git
+cd xt_tls/
+make
+wait
+make install
+wait
+sleep 1
+make dkms-install
+wait
+echo "done:"
+echo "curl -v --tlsv1.3 https://www.facebook.com/"
+echo 'iptables -A OUTPUT -p tcp --dport 443 -m tls --tls-host "*.facebook.com" -j DROP'
+echo 'iptables -A OUTPUT -p tcp --dport 443 -m tls --tls-host "*.googlevideo.com" -j DROP'
+echo 'iptables -A OUTPUT -p tcp --dport 443 -m tls --tls-host "*.fbcdn.net" -j DROP'
